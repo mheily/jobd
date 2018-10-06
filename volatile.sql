fe3156c59de17310db6df5289033d5508a934c56
Mark Heily <mark@heily.com>
2018-10-06 12:49:16 -0400

    Add missing schema changes to previous commit.

--- volatile.sql
+++ volatile.sql
@@ -6,6 +6,10 @@ BEGIN TRANSACTION;
 CREATE TABLE volatile.processes (
     pid INTEGER PRIMARY KEY,    -- matches kernel PID
     job_id INTEGER NOT NULL,
+    exited INTEGER CHECK (exited IN (0,1)),
+    exit_status INTEGER,
+    signaled INTEGER CHECK (signaled IN (0,1)),
+    signal_number INTEGER,
     process_state_id INTEGER NOT NULL DEFAULT 1,
     FOREIGN KEY (process_state_id) REFERENCES process_states (id) ON DELETE RESTRICT
     -- FIXME: will not work due to sqlite limitation: FOREIGN KEY (job_id) REFERENCES main.jobs (id) ON DELETE RESTRICT
@@ -22,19 +26,21 @@ INSERT INTO process_states (id, name) VALUES (4, 'stopping');
 INSERT INTO process_states (id, name) VALUES (5, 'stopped');
 INSERT INTO process_states (id, name) VALUES (6, 'error');
 
--- CREATE VIEW IF NOT EXISTS process_table_view
+-- CREATE VIEW IF NOT EXISTS volatile.process_table_view
 -- AS
 -- SELECT 
---   jobs.id,
---   jobs.job_id,
+--   main.jobs.id,
+--   main.jobs.job_id,
 --   processes.pid AS pid,
 --   processes.process_state_id AS state_id,
---   process_states.name AS state
--- FROM jobs 
+--   process_states.name AS state,
+--   processes.exit_status,
+--   processes.signal_number
+-- FROM main.jobs 
 -- INNER JOIN processes ON jobs.id = processes.job_id 
 -- INNER JOIN process_states on processes.process_state_id = process_states.id
 -- ORDER BY jobs.job_id;
-
+ 
 --- configuration changes requested via jobadm(1)
 
 CREATE TABLE volatile.pending_changes (
