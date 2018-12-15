ffb4ff550b185235479b596c623d5baa11c6c144
Mark Heily <mark@heily.com>
2018-12-15 13:57:03 -0500

    Make system manager the default

--- test/vagrant.sh
+++ test/vagrant.sh
@@ -10,7 +10,7 @@ if [ "$1" = "inside-the-box" ] ; then
     # WORKAROUND: rsync blows away a bunch of stuff in the tree
     # maybe out of tree builds are good :)
     make distclean
-    ./configure --as-system-manager
+    ./configure
     ./test/build.sh
     ./test/run.sh
 
