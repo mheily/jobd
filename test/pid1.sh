626321aa50f465474800fa34fbd235eda75a08e2
Mark Heily <mark@heily.com>
2018-12-15 13:58:14 -0500

    Fix pid1 test

--- test/pid1.sh
+++ test/pid1.sh
@@ -4,18 +4,17 @@
 #
 
 if [ "$1" = "inside-the-box" ] ; then
+    sudo pkill -9 jobd
     set -ex
     cd /vagrant
 
     make distclean
-    ./configure --as-system-manager
+    ./configure
     make
-    make init
     sudo make install
-    sudo rm -f /lib/jmf/var/jmf/repository.db
+    sudo rm -f /lib/jobd/repository.db
     sudo jobcfg init
-    rm -f /tmp/*.toml
-    cat >/tmp/rc.toml <<EOF
+    sudo jobcfg -v import <<EOF
 name = "rc"
 exclusive = true
 standard_out_path = "/dev/console"
@@ -26,21 +25,18 @@ start = "/bin/sh -x /etc/rc autoboot"
 stop = "/bin/sh -x /etc/rc.shutdown"
 EOF
    
-    cat >/tmp/reopen_db.toml <<EOF
+    sudo jobcfg -v import <<EOF
 name = "jobd/db_reopen"
 exclusive = true
 command = "/bin/jobadm jobd reopen_database"
 after = "rc"
 EOF
-    sudo rm -f /lib/jmf/share/jmf/manifests/*
-    sudo mv /tmp/*.toml /lib/jmf/share/jmf/manifests
-    sudo jobcfg -f /lib/jmf/share/jmf/manifests -v import
-    echo 'init_path="/lib/jmf/sbin/init"' | sudo tee /boot/loader.conf
-    sudo reboot
+
+    echo 'init_path="/lib/jobd/init"' | sudo tee /boot/loader.conf.local
     exit
 fi
 
 set -ex
 test -e Vagrantfile
 vagrant rsync
-vagrant ssh -c '/vagrant/test/pid1.sh inside-the-box'
+vagrant ssh -c '/vagrant/test/pid1.sh inside-the-box && sudo reboot'
